5c1958c65b09894ede5a03103becec29eb4c648d
aldissa <aldis.a101@gmail.com>
2026-06-25 10:30:51 +0700

    feat: setup backend Django + import data CSV + struktur frontend

--- backend/pengetahuan/admin.py
+++ backend/pengetahuan/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Penyakit, Gejala, RelasiPenyakitGejala
 
-# Register your models here.
+@admin.register(Penyakit)
+class PenyakitAdmin(admin.ModelAdmin):
+    list_display = ['id_penyakit', 'nama_penyakit']
+    search_fields = ['nama_penyakit']
+
+@admin.register(Gejala)
+class GejalaAdmin(admin.ModelAdmin):
+    list_display = ['kode_gejala', 'nama_gejala']
+    search_fields = ['kode_gejala', 'nama_gejala']
+
+@admin.register(RelasiPenyakitGejala)
+class RelasiAdmin(admin.ModelAdmin):
+    list_display = ['id', 'id_penyakit', 'kode_gejala', 'mb', 'md', 'cf_pakar']
+    list_filter = ['id_penyakit']
